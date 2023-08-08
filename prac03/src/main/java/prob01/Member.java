c9fb80e6f67db396fa177114976a547b428a9219
YeJi222 <hhongyizi@gmail.com>
2023-08-08 17:34:16 +0900

    feat: practice03-Prob01 Member.java code

---
+++ prac03/src/main/java/prob01/Member.java
@@ -0,0 +1,27 @@
+package prob01;
+
+public class Member {
+	private String id;
+	private String name;
+	private double point;
+	
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getPoint() {
+		return point;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPoint(double point) {
+		this.point = point;
+	}
+}
