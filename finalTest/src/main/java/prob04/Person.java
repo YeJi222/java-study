910feed4e5c9785fa76df9768e1c290d3505e592
YeJi222 <hhongyizi@gmail.com>
2023-08-09 16:54:28 +0900

    feat: finalTest-Prob04 code

--- finalTest/src/main/java/prob04/Person.java
+++ finalTest/src/main/java/prob04/Person.java
@@ -6,4 +6,21 @@ public class Person {
 	private String name;
 	
 	/* 코드 작성 */
+	public Person(String name) { // Constructor 
+		this.name = name;
+		this.age = 12;
+		numberOfPerson += 1;
+	}
+	public Person(int age, String name) { // 오버로딩 
+		this.age = age;
+		this.name = name;
+		numberOfPerson += 1;
+	}
+	
+	public void selfIntroduce(){
+		System.out.println("내 이름은 " + name + "이며, 나이는 " + age + "입니다.");
+	}
+	public static int getPopulation() {
+		return numberOfPerson;
+	}
 }
