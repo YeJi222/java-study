570670b5f2e5282dcae5fbfaaae22ce71f0b5e7d
YeJi222 <hhongyizi@gmail.com>
2023-08-09 10:46:07 +0900

    feat: practice03-Prob02 code

---
+++ prac03/src/main/java/prob02/Goods.java
@@ -0,0 +1,28 @@
+package prob02;
+
+public class Goods {
+	private String name;
+	private int money;
+	private int num;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public int getNum() {
+		return num;
+	}
+	
+}
--- prac03/src/main/java/prob02/GoodsApp.java
+++ prac03/src/main/java/prob02/GoodsApp.java
@@ -8,12 +8,29 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		Goods[] goods = new Goods[COUNT_GOODS];
+		Goods[] goods = new Goods[COUNT_GOODS]; // 객체 배열
+		
+		// 인스턴스화 
+		for(int i = 0 ; i < goods.length ; i++) {
+			goods[i] = new Goods();
+		}
 
 		// 상품 입력
-		
+		for(int i = 0 ; i < goods.length ; i++) {
+			String input = scanner.nextLine();
+			String[] inputSplit = input.split(" ");
+			
+			goods[i].setName(inputSplit[0]);
+			goods[i].setMoney(Integer.parseInt(inputSplit[1]));
+			goods[i].setNum(Integer.parseInt(inputSplit[2]));
+		}
 
-		// 상품 출
+		// 상품 출력 
+		System.out.println();
+		for(int i = 0 ; i < goods.length ; i++) {
+			System.out.println(goods[i].getName() + "(가격: " + 
+					goods[i].getMoney() + ")이 " + goods[i].getNum() + "개 입고 되었습니다.");
+		}
 		
 		// 자원정리
 		scanner.close();
