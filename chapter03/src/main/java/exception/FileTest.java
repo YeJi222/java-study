2ea9fc9e2fa5ff93b3c54023487d9d859ca53cba
YeJi222 <hhongyizi@gmail.com>
2023-08-10 15:56:45 +0900

    feat: lecture-execption

--- chapter03/src/main/java/exception/FileTest.java
+++ chapter03/src/main/java/exception/FileTest.java
@@ -2,13 +2,26 @@ package exception;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileTest {
 	public static void main(String[] args) {
+		FileInputStream fis = null;
 		try {
-			FileInputStream fs = new FileInputStream("hello.txt");
+			fis = new FileInputStream("hello3.txt");
+			int data = fis.read();
+			
+			System.out.println((char)data);
 		} catch (FileNotFoundException e) {
 			System.out.println("error: " + e);
+		} catch(IOException e) {
+			System.out.println("error: " + e);
+		} finally { // 자원정리 
+			try {
+				if(fis != null) fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
