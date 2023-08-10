0d86603fd953562bc04733014b82e94933c5054d
YeJi222 <hhongyizi@gmail.com>
2023-08-10 22:06:53 +0900

    feat: practice04-Prob01 code

--- prac04/src/main/java/prob01/Printer.java
+++ prac04/src/main/java/prob01/Printer.java
@@ -2,4 +2,18 @@ package prob01;
 
 public class Printer {
 
+	public void println(int intVal) {
+		System.out.println(intVal);
+	}
+	public void println(Boolean boolVal) {
+		System.out.println(boolVal);
+		
+	}
+	public void println(double doubleVal) {
+		System.out.println(doubleVal);
+	}
+	public void println(String strVal) {
+		System.out.println(strVal);
+	}
+
 }
