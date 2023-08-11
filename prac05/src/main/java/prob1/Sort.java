09a34e5f5f91d12359650b1049c43abcb909c5e6
YeJi222 <hhongyizi@gmail.com>
2023-08-11 09:25:34 +0900

    feat: practice05-Prob1 code

---
+++ prac05/src/main/java/prob1/Sort.java
@@ -0,0 +1,36 @@
+package prob1;
+
+public class Sort {
+	
+	public static void main(String[] arg) {
+	
+		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
+		int count =  array.length;
+		
+		System.out.println( "Before sort." );
+		
+		for (int i = 0; i < count; i++) {
+			System.out.print( array[ i ] + " " );
+		}
+		
+		//
+		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
+		// Bubble Sort
+		for(int k = 0 ; k < array.length - 1 ; k++) {
+			for(int i = 0 ; i < array.length - 1 ; i++) {
+				if(array[i] < array[i+1]) {
+					int temp = array[i];
+					array[i] = array[i+1];
+					array[i+1] = temp;
+				}
+			}
+		}
+		
+		// 결과 출력
+		System.out.println( "\nAfter Sort." );
+		
+		for (int i = 0; i < count; i++) {
+			System.out.print(array[i] + " ");
+		}		
+	}
+}
