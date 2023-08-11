d9a8cdd72c69ae248b69ad33ed4fb8bb0a1cc396
YeJi222 <hhongyizi@gmail.com>
2023-08-11 23:58:50 +0900

    feat: practice05-Prob6 code

---
+++ prac05/src/main/java/prob6/RectTriangle.java
@@ -0,0 +1,12 @@
+package prob6;
+
+public class RectTriangle extends Shape {
+	private double width;
+	private double height;
+
+	public RectTriangle(double w, double h) {
+		this.width = w;
+		this.height = h;
+	}
+
+}
---
+++ prac05/src/main/java/prob6/Shape.java
@@ -0,0 +1,14 @@
+package prob6;
+
+public abstract class Shape {
+
+	public String getArea() {
+		return (width * height).toString();
+	}
+
+	public String getPerimeter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
