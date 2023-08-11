aff17e2c243c797b54456495c65e051fc47487e8
YeJi222 <hhongyizi@gmail.com>
2023-08-11 17:05:18 +0900

    feat: practice05-Prob02 code

--- prac05/src/main/java/prob2/MusicPhone.java
+++ prac05/src/main/java/prob2/MusicPhone.java
@@ -1,17 +1,18 @@
 package prob2;
 
 public class MusicPhone extends Phone {
-	  public void execute ( String function ) {
-	      if (function.equals("음악") ) {
-	          playMusic();
-	          return;
-	      }
+	@Override
+	public void execute ( String function ) {
+		if (function.equals("음악") ) {
+	         playMusic();
+	         return;
+	     }
 	      
-	      super.execute( function );
+	     super.execute( function );
 	}
 
 	  private void playMusic(){
-	    System.out.print("MP3 플레이어에서 음악재생");
+		  System.out.println("MP3 플레이어에서 음악재생");
 	  }
-	}
+}
 
