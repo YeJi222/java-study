35638672ec743f674759b662012812db4348f07f
YeJi222 <hhongyizi@gmail.com>
2023-08-16 17:39:46 +0900

    feat: finalTest-Prob03 code(finished)

--- finalTest/src/main/java/prob03/Money.java
+++ finalTest/src/main/java/prob03/Money.java
@@ -1,10 +1,71 @@
 package prob03;
 
+import java.util.Objects;
+
 public class Money {
 	private int amount;
 	
 	/* 코드 작성 */
+	public Money() {
+	}
+	
 	public Money(int num) {
+		this.amount = num;
+	}
+
+	public Money add(Money amount) {
+		// 자신의 금액과 인자로 넘어온 Money 객체의 금액(amount) 계산 
+		int temp = this.amount;
+		this.amount += amount.amount;
+		// 계산된 금액으로 새로운 Money 객체로 생성하여 리턴 
+		Money money = new Money(this.amount);
+		this.amount = temp;
 		
+		return money;
+	}
+	
+	public Money minus(Money amount) {
+		int temp = this.amount;
+		this.amount -= amount.amount;
+		Money money = new Money(this.amount);
+		this.amount = temp;
+				
+		return money;
+	}
+	
+	public Money multiply(Money amount) {
+		int temp = this.amount;
+		this.amount *= amount.amount;
+		Money money = new Money(this.amount);
+		this.amount = temp;
+		
+		return money;
+	}
+	
+	public Money divide(Money amount) {
+		int temp = this.amount;
+		this.amount /= amount.amount;
+		Money money = new Money(this.amount);
+		this.amount = temp;
+		
+		return money;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		
+		// 인자로 넘어온 Object 객체가 Money 타입인지를 확인
+		if (getClass() == obj.getClass()) {
+			Money other = (Money) obj;
+			if(amount == other.amount) return true;
+		} 
+		return false;
 	}
 }
--- finalTest/src/main/java/prob03/MoneyTest.java
+++ finalTest/src/main/java/prob03/MoneyTest.java
@@ -7,11 +7,15 @@ public class MoneyTest {
 		Money two = new Money(2);
 		Money three = new Money(3);
 		Money ten = new Money(10);
+		
+		System.out.println(five);
+		System.out.println(two.add(three));
+		System.out.println(five.equals(two.add(three)));
 
 		if (five.equals(two.add(three))
 				&& three.equals(five.minus(two))
 				&& ten.equals(five.multiply(two))
-				&& two.equals(ten.devide(five))) {
+				&& two.equals(ten.divide(five))) {
 			System.out.println("Money Class 구현을 완료 하였습니다.");
 		}
 	}
