77c30f00b104d564a322a30be4f39070a835e382
YeJi222 <hhongyizi@gmail.com>
2023-08-16 12:38:04 +0900

    feat: lecture-dataStructure

--- chapter04/src/main/java/collection/Gugudan.java
+++ chapter04/src/main/java/collection/Gugudan.java
@@ -1,9 +1,37 @@
 package collection;
 
-public class Gugudan {
+import java.util.Objects;
 
-	public Gugudan(int i, int j) {
-		
+public class Gugudan {
+	private int lValue;
+	private int rValue;
+
+	public Gugudan(int lValue, int rValue) {
+		this.lValue = lValue;
+		this.rValue = rValue;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(lValue * rValue);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gugudan other = (Gugudan) obj;
+		return lValue * rValue == other.lValue * other.rValue;
+	}
+
+	@Override
+	public String toString() {
+		return "Gugudan [lValue=" + lValue + ", rValue=" + rValue + "]";
+	}
+
+	
 }
